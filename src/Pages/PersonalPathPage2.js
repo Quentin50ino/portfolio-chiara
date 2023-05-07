6641f759f980c5cc80dbe7038a9044e260c9d8c4
Alberto Bollino <alberto.bollino@outlook.it>
2023-05-07 16:29:21 +0200

    deleted all svg images (now we take it from internet

--- src/Pages/PersonalPathPage2.js
+++ src/Pages/PersonalPathPage2.js
@@ -1,22 +1,22 @@
 import { useNavigate } from 'react-router';
 import '../App.css';
-import arrowLeft from '../img/arrow-left.svg';
-import pattern1 from '../img/pattern 1.svg';
-import pattern1b from '../img/pattern 1 b.svg';
-import pattern2 from '../img/Pattern 2.svg';
-import pattern2b from '../img/Pattern 2b.svg';
-import pattern3 from '../img/Pattern 3.svg';
-import pattern3b from '../img/Pattern 3b.svg';
-import pattern4 from '../img/Pattern 4.svg';
-import pattern4b from '../img/Pattern 4b.svg';
-import pattern5 from '../img/Pattern 5.svg';
-import pattern5b from '../img/Pattern 5b.svg';
-import lineaRossa from '../img/Linea rossa.svg';
-
 
 function PersonalPathPage2() {
     
     const navigate = useNavigate();
+
+    let arrowLeft = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/arrow-left.svg?token=ATJMFNSFK3WWZ77QBPGP4TDEK6ZFC";
+    let lineaRossa = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Linea%20rossa.svg?token=ATJMFNRFBEB26YG5CYXTUOLEK6ZCY";
+    let pattern1 = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/pattern%201.svg?token=ATJMFNXRRMUGFQDETAJRMWLEK6YW6";
+    let pattern1b = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/pattern%201%20b.svg?token=ATJMFNQWFSAKADQHYPSPQUDEK6YYI";
+    let pattern2 = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%202.svg?token=ATJMFNXZZAQXMKMXQW7DWALEK6Y5O";
+    let pattern2b = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%202b.svg?token=ATJMFNSKFALJAE7VQNQQR6TEK6Y7G";
+    let pattern3 = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%203.svg?token=ATJMFNQIGDVIZWRJHPH4XYTEK6YIQ";
+    let pattern3b = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%203b.svg?token=ATJMFNT6GDJYHHRYPN7JKE3EK6YMM";
+    let pattern4 = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%204.svg?token=ATJMFNWQ62POUOLJ5DOA7ULEK6YRQ";
+    let pattern4b = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%204b.svg?token=ATJMFNTOSYNQD77JHIPKZMLEK6YTG";
+    let pattern5 = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%205.svg?token=ATJMFNSPS67KBVX32RH22TLEK6YUG";
+    let pattern5b = "https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Pattern%205b.svg?token=ATJMFNRBLB2JTZSCUWSYBKDEK6YVE";
 
   return (
     <div style={{display : 'flex'}}>
@@ -27,36 +27,36 @@ function PersonalPathPage2() {
                 and this is <br></br>
                 my path
                 <div style={{display : 'flex', justifyContent : 'flex-end', marginTop : '20px'}}>
-                    <img src={lineaRossa} width="100px"/>
+                    <img src={lineaRossa} width="100px" alt="red line"/>
                 </div>
             </div>
         </div>
       </div>
       <div style={{width : '50%'}}>
         <div style={{display : 'flex', justifyContent : 'center', marginTop : '100px'}}>
-            <img src={pattern1} width="70px"></img>
+            <img src={pattern1} width="70px" alt="pattern 1"></img>
             <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginTop : '0px', width : '6em'}} className='subtitle'>education</div>
-            <img src={pattern1b} width="70px"></img>
+            <img src={pattern1b} width="70px" alt="pattern 1 b"></img>
         </div>
         <div style={{display : 'flex', justifyContent : 'center', marginTop : '40px'}}>
-            <img src={pattern2} width="70px"></img>
+            <img src={pattern2} width="70px" alt="pattern 2"></img>
             <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginTop : '0px', width : '6em'}} className='subtitle'>experience</div>
-            <img src={pattern2b} width="70px"></img>
+            <img src={pattern2b} width="70px" alt="pattern 2 b"></img>
         </div>
         <div style={{display : 'flex', justifyContent : 'center', marginTop : '40px'}}>
-            <img src={pattern3} width="70px"></img>
+            <img src={pattern3} width="70px" alt="pattern 3"></img>
             <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginTop : '0px', width : '6em'}} className='subtitle'>extras</div>
-            <img src={pattern3b} width="70px"></img>
+            <img src={pattern3b} width="70px" alt="pattern 3 b"></img>
         </div>
         <div style={{display : 'flex', justifyContent : 'center', marginTop : '40px'}}>
-            <img src={pattern4} width="70px"></img>
+            <img src={pattern4} width="70px" alt="pattern 4"></img>
             <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginTop : '0px', width : '6em'}} className='subtitle'>languages</div>
-            <img src={pattern4b} width="70px"></img>
+            <img src={pattern4b} width="70px" alt="pattern 4 b"></img>
         </div>
         <div style={{display : 'flex', justifyContent : 'center', marginTop : '40px'}}>
-            <img src={pattern5} width="70px"></img>
+            <img src={pattern5} width="70px" alt="pattern 5"></img>
             <div style={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginTop : '0px', width : '6em', cursor : 'pointer'}} className='subtitle' onClick={() => navigate('/project-index')}>projects</div>
-            <img src={pattern5b} width="70px"></img>
+            <img src={pattern5b} width="70px" alt="pattern 5 b"></img>
         </div>
       </div>
     </div>
--- src/Pages/ProjectIndexPage.js
+++ src/Pages/ProjectIndexPage.js
@@ -10,43 +10,43 @@ function ProjectIndexPage() {
         key : 'project1',
         name : 'Side to Side',
         subtitle : 'apartment',
-        imageNameFront : 'Progetto 1',
-        imageNameBack : 'Progetto 1b'
+        imageNameFront : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%201.svg?token=ATJMFNVMZLG4NYZ3HZXA3NTEK6ZOQ',
+        imageNameBack : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%201b.svg?token=ATJMFNRKCVU4LOUNVSHXIC3EK6ZTO'
       },
       {
         key : 'project2',
         name : 'Tree Wood',
         subtitle : 'product',
-        imageNameFront : 'Progetto 2 ',
-        imageNameBack : 'Progetto 2b'
+        imageNameFront : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%202%20.svg?token=ATJMFNUCL6DC5WRYCHMFKGDEK6ZWY',
+        imageNameBack : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%202b.svg?token=ATJMFNQD2VSF7YB2U3AHIYLEK6ZYE'
       },
       {
         key : 'project3',
         name : 'Connections',
         subtitle : 'retail',
-        imageNameFront : 'Progetto 3',
-        imageNameBack : 'Progetto 3b'
+        imageNameFront : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%203.svg?token=ATJMFNXMHAVQ6DYAQ5UJQBDEK6ZZK',
+        imageNameBack : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%203b.svg?token=ATJMFNUVQEAR7UIMXEYK3UDEK6Z26'
       },
       {
         key : 'project4',
         name : "Grid's Shadows",
         subtitle : 'office',
-        imageNameFront : 'Progetto 4',
-        imageNameBack : 'Progetto 4b'
+        imageNameFront : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%204.svg?token=ATJMFNRBQIF4CELJB63UAYDEK6Z4E',
+        imageNameBack : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%204b.svg?token=ATJMFNW4EAELEWTA375QPODEK6Z5C'
       },
       {
         key : 'project5',
         name : 'Upstairs',
         subtitle : 'boutique hotel',
-        imageNameFront : 'Progetto 5',
-        imageNameBack : 'Progetto 5b'
+        imageNameFront : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%205.svg?token=ATJMFNSNHND4SHZNIGVZEL3EK6Z6G',
+        imageNameBack : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%205b.svg?token=ATJMFNRIC3ADLXL6YMPJ4O3EK6Z7I'
       },
       {
         key : 'project6',
         name : 'Kitchen',
         subtitle : 'apartment',
-        imageNameFront : 'Progetto 6',
-        imageNameBack : 'Progetto 6b'
+        imageNameFront : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%206.svg?token=ATJMFNS2IY7O3HYUGDT7TRDEK62AI',
+        imageNameBack : 'https://raw.githubusercontent.com/Quentin50ino/portfolio-chiara-images/d43a50f8376526fb51f61e0aff38c9cd2aeb2873/Progetto%206b.svg?token=ATJMFNTI4JHFO5MKXHQPQADEK62BI'
       }
     ]
 
@@ -60,12 +60,12 @@ function ProjectIndexPage() {
           projects.map((project) => {
             return(
               <div style={{display : 'flex', justifyContent : 'center', marginTop : '30px'}}>
-                <img key={project.key} src={require(`../img/${project.imageNameFront}.svg`)} width="50px"></img>
+                <img key={project.key} src={project.imageNameFront} width="50px" alt="project front"></img>
                 <div style={{display : 'flex', flexDirection : 'column', justifyContent : 'center', alignItems : 'center', marginTop : '0px', width : '8em'}} className='subtitle'>
                     <div style={{fontSize : '18px'}}>{project.name}</div>
                     <div style={{fontSize : '12px'}}>{project.subtitle}</div>
                   </div>
-                <img key={project.key} src={require(`../img/${project.imageNameBack}.svg`)} width="12px"></img>
+                <img key={project.key} src={project.imageNameBack} width="12px" alt="project back"></img>
             </div>
             )
           })
